3d4324dc48a71406838be25dae9c258d9560975f
Shane <gupitshanehazel@gmail.com>
2023-04-07 03:07:29 +0800

    Assignment 2

---
+++ PROBLEM #2.py
@@ -0,0 +1,33 @@
+# pseudocode
+
+# make user enter their string
+input_str = input("Enter your string: ")
+output_str = ""
+
+# check every character
+for i in range(len(input_str)):
+
+# if *, change a
+    if input_str[i] == "*":
+        output_str += "a"
+
+# else, if &, change e
+    elif input_str[i] == "&":
+        output_str +="e"
+
+# else, if #, change i
+    elif input_str[i] == "#":
+        output_str +="i"
+
+# else, if +, change o
+    elif input_str[i] == "+":
+        output_str +="o"
+
+# else, if !, change u
+    elif input_str[i] == "!":
+        output_str +="u"
+    else:
+        output_str += input_str[i]
+
+# print output
+print(output_str)
---
+++ PROBLEM #3.py
@@ -0,0 +1,35 @@
+# pseudocode
+
+# ask user for plaintext and keyword (all caps, no spaces)
+plaintext_str = input("Enter the message to encrypt: ")
+keyword_str = input("Enter the keyword to encrypt: ")
+
+# define cipher string
+def cipher_str(plaintext_str, keyword_str):
+
+# make the plaintext and keyword all caps and no spaces
+    plaintext_str = plaintext_str.upper().replace("","")
+    keyword_str = keyword_str.upper().replace("","")
+
+# store the value in cipher code
+    ciphercode = ""
+
+# check every character in the plaintext
+    for i in range(len(plaintext_str)):
+
+# translate letter to its corresponding nuumber
+        plaintext_int = ord(plaintext_str[i]) - 65
+        keyword_int = ord(keyword_str[i % len(keyword_str)]) - 65
+
+# encrypt the plaintext with the given keyword
+        ciphercode_int = (plaintext_int + keyword_int) % 26
+
+# translate number to its corresponding letter
+        ciphercode += chr(ciphercode_int + 65)
+    return ciphercode
+
+# convert cipher text int to str
+ciphercode = cipher_str(plaintext_str, keyword_str)
+
+# print output
+print("Ciphertext: ", ciphercode)
